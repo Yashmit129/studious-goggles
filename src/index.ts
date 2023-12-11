63609580b784cf0da0507a2661d178e26a017602
YASHMIT SAGARI <yashmitsagari@gmail.com>
2023-12-11 18:23:00 +0530

    AAA-000 | Yashmit Sagari | changes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import * as fs from "node:fs";
-(async function () {
-    await fs.writeFile('m', "hello world!", (err) => {
-        if (err) console.log(err.message); else console.log('Done!')
-    })
-})()
+await fs.writeFile('m', "hello world!", (err) => {
+    if (err) console.log(err.message); else console.log('Done!')
+})
